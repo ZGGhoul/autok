054995c5877926d8b8797af45257fb55184fd48f
ZGGhoul <134587569+ZGGhoul@users.noreply.github.com>
2026-01-30 11:50:00 +0100

    Update program.js

--- program.js
+++ program.js
@@ -3,7 +3,7 @@ import { legjobb } from "./autok.js";
 console.log("Sziaa Goth lány");
 
 for (let i=1; i<=7; i++) console.log(auto);
-console.log("Függvények")
+console.log("Függvények hívása")
 legjobb()
 legjobb()
 
